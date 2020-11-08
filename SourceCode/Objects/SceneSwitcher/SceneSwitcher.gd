38e052e41bd9582bf63902d77d54952d080be473
mightyminebuster <56921315+mightyminebuster@users.noreply.github.com>
2020-11-08 03:54:05 -0500

    Scene Swithcer still dosent work

--- SourceCode/Objects/SceneSwitcher/SceneSwitcher.gd
+++ SourceCode/Objects/SceneSwitcher/SceneSwitcher.gd
@@ -6,7 +6,7 @@ func change_scene(scene: PackedScene, delay: int = 0.5) -> void:
 	yield(get_tree().create_timer(delay), "timeout")
 	$AnimationPlayer.play("FadeToBlack")
 	yield($AnimationPlayer, "animation_finished")
-	get_tree().change_scene_to(scene)
+	assert(get_tree().change_scene_to(scene) == OK)
 	$AnimationPlayer.play("FadeFromBlack")
 	yield($AnimationPlayer,"animation_finished")
 	emit_signal("scene_changed")
