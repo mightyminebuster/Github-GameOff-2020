b08660a30131f0b44a4bafd192e59fe1ef71ec3e
mightyminebuster <56921315+mightyminebuster@users.noreply.github.com>
2020-11-10 16:56:43 -0500

    Camera Rotation fix

--- SourceCode/Objects/Obstacles/SecurityCamera/SecurityCamera.gd
+++ SourceCode/Objects/Obstacles/SecurityCamera/SecurityCamera.gd
@@ -1,7 +1,13 @@
 extends Position2D
+export var speed: float = 1
 
 func _physics_process(_delta : float) -> void:
-	for raycast in find_node("Raycasts").get_children():
-		if raycast.is_colliding():
-			if raycast.get_collider().collision_layer == 2147483652:
-				raycast.get_collider().set_state("die")
+	if !Engine.editor_hint:
+		for raycast in find_node("RotationFix").find_node("Raycasts").get_children():
+			if raycast.is_colliding():
+				if raycast.get_collider().collision_layer == 2147483652:
+					raycast.get_collider().set_state("die")
+
+
+func _process(delta):
+	$RotationFix/AnimationPlayer.playback_speed = speed
