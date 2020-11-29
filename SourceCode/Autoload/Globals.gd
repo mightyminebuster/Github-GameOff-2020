cd1be590c2f5acd6cd3e1828baf7efc545805a56
mightyminebuster <56921315+mightyminebuster@users.noreply.github.com>
2020-11-29 16:27:29 -0500

    added cheese variation to level 2

--- SourceCode/Autoload/Globals.gd
+++ SourceCode/Autoload/Globals.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var player_default_position: Vector2 = Vector2.ZERO
+var player_default_position: Vector2 = Vector2(64, 64)
 var has_grappling_hook: bool = false
 
 var player_default_gravity: int = 1700
